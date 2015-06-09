d0eb019f7fd41b995cd12a786c643ab0319021cd
htahir1 <htahir111@gmail.com>
2015-06-09 16:48:05 +0500

    Fixed big in detail activity

--- app/src/main/java/com/hamza_tahir/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/hamza_tahir/sunshine/DetailActivityFragment.java
@@ -3,6 +3,7 @@ package com.hamza_tahir.sunshine;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,10 +21,13 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
         Intent intent = getActivity().getIntent();
         String forecast = intent.getStringExtra("forecast");
-        TextView forecastView = (TextView) container.findViewById(R.id.textView_forecastDetail);
+        TextView forecastView = (TextView) rootView.findViewById(R.id.textView_forecastDetail);
+        //Log.v("Forecast", forecast);
         forecastView.setText(forecast);
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        return rootView;
     }
 }
